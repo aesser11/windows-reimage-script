cf3ff9b657af4051bbe10122096403d4277a22bb
aesser11 <9578875+aesser11@users.noreply.github.com>
2020-05-07 12:46:46 -0500

    inital setup copying stuff over

--- Windows10PostDeploy-AllUsers.ps1
+++ Windows10PostDeploy-AllUsers.ps1
@@ -1,13 +1,16 @@
-# Windows10PostDeploy-AllUsers.ps1
-
-# this script is for all users
-
-
-# Lists
-###################
-# Windows 10 Apps #
-###################
-$regularUserWin10AppBlacklist = @(
+####################################
+# Windows10PostDeploy-AllUsers.ps1 #
+####################################
+try {
+# Relaunch the script with administrator privileges and bypass execution-policy if it isn't already
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File $PSCommandPath" -Verb RunAs
+    exit
+}
+################################
+# Windows 10 Apps to Blacklist #
+################################
+$win10AppBlacklist = @(
     # non-microsoft as of 1909
     "*Solitaire*",
     "*SkypeApp*",
@@ -61,44 +64,138 @@ $regularUserWin10AppBlacklist = @(
 # 3rd Party Apps to Install #
 #############################
 $applicationsToInstall = @(
-    # my chocolatey apps
-    "7zip",
+    # standard apps ever windows instance should have
+    "7zip",#no pin && #append-software
+    "windirstat",#no pin
     "hwinfo",
-    "msiafterburner",
-    "powertoys",
-    "windirstat",
-    "rufus",
-    "putty",
-    "github-desktop",
-    "sublimetext3",
-    # alternative to sublime
     "notepadplusplus",
-    "teamviewer",
-    "vlc",
+    #vlc
+    #rufus
+
+    #choice: a,b,none
     "googlechrome",
-    # alternative to chrome
-    "firefox",
-    "spotify",
-    "discord",
-    "steam",
-    "goggalaxy",
-    "origin",
-    "uplay",
-    "epicgameslauncher"
-
-    # manual installs
-    # "battle.net"
-)
-
-$firstRunFunctions = @(
+    "firefox"
 
 )
 
-$allUsersFunctions = @(
+#################
+# Reimage Steps #
+#################
+$firstRunFunctions1 = @(
 
 )
 
-$regularUserFunctions = @(
+$finalFirstRunFunctions3 = @(
 
 )
+
+$everyRunFunctions2 = @(
+
+)
+
+$finalEveryRunFunctions4 = @(
+
+)
+
+#############
+# Functions #
+#############
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################
+# Restart Prompt #
+##################
+Function promptForRestart {
+    Switch (Read-Host "Restart? [y]/[n]") {
+        'y' {Restart-Computer}
+        'n' {Write-host "Exiting" -ForegroundColor Yellow}
+        default {
+            Write-host "Invalid input. Please enter [y]/[n]" -ForegroundColor Red
+            promptForRestart
+        }
+    }
+}
+
+####################
+# Output Text File #
+####################
+Function remainingStepsToText {
+    $outputFile = "C:\Users\$env:username\Desktop\Remaining Steps.txt"
+    $outString = "
+################
+# Manual Steps #
+################
+# set task manager startup apps to disabled from running at boot
+# unpin default groups from start menu
+
+#########################
+# Appended Output Steps #
+#########################
+$global:appendOutputSteps
+###################################################################################################
+###########################
+# Appended Software Steps #
+###########################
+$global:appendOutputSoftware
+###################################################################################################
+##############
+# Error List #
+##############
+$error
+"
+    $outString | Out-File -FilePath $outputFile -Width 200
+}
+
+####################
+# Global Variables #
+####################
+$global:appendOutputSteps = $null
+$global:appendOutputSoftware = $null
+
+##################
+# Function Calls #
+##################
+Function promptFreshInstall {
+    Switch (Read-Host "Fresh Install? [y]/[n]") {
+        'y' {
+            Write-host "Including first install steps..." -ForegroundColor Yellow
+            $firstRunFunctions1 | ForEach { Invoke-Expression $_ }
+            $everyRunFunctions2 | ForEach { Invoke-Expression $_ }
+            $finalFirstRunFunctions3 | ForEach { Invoke-Expression $_ }
+            $finalEveryRunFunctions4 | ForEach { Invoke-Expression $_ }
+        }
+        'n' {
+            Write-host "Skipping first install steps..." -ForegroundColor Yellow
+            $everyRunFunctions2 | ForEach { Invoke-Expression $_ }
+            $finalEveryRunFunctions4 | ForEach { Invoke-Expression $_ }
+         }
+        default {
+            Write-host "Invalid input. Please enter [y]/[n]" -ForegroundColor Red
+            promptFreshInstall
+        }
+    }
+}
+promptFreshInstall
+}
+catch {
+    $error | Out-File -FilePath "C:\Users\$env:username\Desktop\CrashLog.txt" -Width 200
+}
+
 
